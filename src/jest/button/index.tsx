4a3110c4bff632e0d406598efb4ce231756c43f9
RYOTA-F <ry.fj0304@gmail.com>
2022-09-10 12:31:39 +0900

    :wrench: Jest

---
+++ src/jest/button/index.tsx
@@ -0,0 +1,14 @@
+import { FC, ReactNode } from 'react'
+
+export type ButtonProps = {
+  children: ReactNode
+  onClick: () => void
+}
+
+export const Button: FC<ButtonProps> = ({ children, onClick }) => {
+  return (
+    <button type="button" onClick={onClick}>
+      {children}
+    </button>
+  )
+}
