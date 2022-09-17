5d6c4e9e95d2596f2e87893762c590e5710094a9
RYOTA-F <ry.fj0304@gmail.com>
2022-09-17 17:31:08 +0900

    :sparkles: Recoil

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,18 +1,22 @@
 import { FC } from 'react'
+import { useRecoilValue } from 'recoil'
 /* components */
 import SignInButton from '../../components/signInButton'
 import SignOutButton from '../../components/signOutButton'
 import UserInfo from '../../components/userInfo'
 /* libs */
-import { useIsSigned } from '../../lib/auth/google'
+import authState from '../../lib/recoil/atom/auth'
 /* styles */
 import { LoginContainer } from './style'
 
 const Login: FC = () => {
+  const authUser = useRecoilValue(authState)
+
   return (
     <LoginContainer>
       <h1>Login</h1>
-      {useIsSigned() ? (
+
+      {authUser ? (
         <>
           <UserInfo />
           <div>
