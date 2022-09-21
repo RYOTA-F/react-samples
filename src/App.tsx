49f59925a38702a4d130f3a6e37ddaf77b011484
RYOTA-F <ry.fj0304@gmail.com>
2022-09-21 16:05:29 +0900

    :sparkles: recoilize

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import { RecoilRoot } from 'recoil'
+import RecoilizeDebugger from 'recoilize'
 import Router from './router'
 import { AppContainer } from './App.style'
 
@@ -7,6 +8,7 @@ const App: FC = () => {
   return (
     <AppContainer>
       <RecoilRoot>
+        <RecoilizeDebugger />
         <Router />
       </RecoilRoot>
     </AppContainer>
