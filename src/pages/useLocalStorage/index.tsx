6230f1fc3a298bd5dd60e63c32b694ea7d6379bb
RYOTA-F <ry.fj0304@gmail.com>
2022-09-26 19:30:34 +0900

    :recycle: UseLocalStorage Style

--- src/pages/useLocalStorage/index.tsx
+++ src/pages/useLocalStorage/index.tsx
@@ -1,8 +1,11 @@
 import { FC } from 'react'
 /* components */
 import PageTitle from '../../components/pageTitle'
+import Button from '../../components/button'
 /* libs */
 import useLocalStorage from '../../lib/hooks/useLocalStorage'
+/* styles */
+import { Count } from './style'
 
 const UseLocalStorage: FC = () => {
   const [value, setValue] = useLocalStorage('test', 0)
@@ -10,10 +13,10 @@ const UseLocalStorage: FC = () => {
   return (
     <>
       <PageTitle title="LocalStorage" />
-      <div>{value}</div>
-      <div>
-        <button onClick={() => setValue(value + 1)}>+</button>
-      </div>
+      <Count>{value}</Count>
+      <Button childlen="+" onClick={() => setValue(value + 1)} />
+      <Button childlen="Clear" onClick={() => setValue(0)} marginLeft={20} />
+      <Button childlen="-" onClick={() => setValue(value - 1)} marginLeft={20} />
     </>
   )
 }
---
+++ src/pages/useLocalStorage/style.tsx
@@ -0,0 +1,7 @@
+import style from 'styled-components'
+
+export const Count = style.div`
+  margin-top: 20px;
+  margin-bottom: 60px;
+  font-size: 30px;
+`
